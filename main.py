90ede578e0c988aba62516cd7ac2219abaf23caf
Jeffrey Smith <jeffrey_s_smith@live.com>
2023-02-24 21:38:38 -0800

    added purchase, read, write file

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ while again.upper() == "Y":
     b = purchase.purchase(a)
     write.over_write(a,b)
     again = input("\n Is there any new customers waiting to buy product? ")
-print("\nThank you for shopping from Pi on the Wall!")
-print ("Please check ")
+print("\nThank you for shopping from Pi on the Wall ELECTRONIC STORE!")
+print ("Please check invoice for shopping details, \nWhich we have created in .txt file format.")
